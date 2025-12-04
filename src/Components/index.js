cd8e71ef775990177024580381bee1fa21cf2493
Mitchell Schooler <mitchell@schooler.me>
2025-12-04 10:39:25 -0800

    Updates

--- src/Components/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Copyright from './Copyright';
-import Footer from './Footer';
-import Header from './Header';
-import Hero from './Hero';
-import TopicCard from './TopicCard';
-
-export { Copyright, Footer, Header, Hero, TopicCard };
